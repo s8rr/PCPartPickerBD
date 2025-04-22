f63d1801bddb83a1461a4393e5d13db6951f05cd
apbnschool <rajjak@apbn.edu.bd>
2025-04-22 12:04:42 +0600

    testing github action

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,7 +4,7 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center space-x-2">
       <span className="font-bold text-xl">
-        <span className="text-primary">PCPart</span>PickerBD
+        <span className="text-primary">PCPart</span>PickerBD wow
       </span>
     </Link>
   )
